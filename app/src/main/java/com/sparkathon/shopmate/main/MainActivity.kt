da9cdd4603045c2c34aecb15b6ff2e63064f2116
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2024-08-16 02:34:30 +0530

    test

--- app/src/main/java/com/sparkathon/shopmate/main/MainActivity.kt
+++ app/src/main/java/com/sparkathon/shopmate/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.Ndef
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,7 +15,7 @@ import com.sparkathon.shopmate.ui.theme.ShopMateTheme
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var nfcAdapter: NfcAdapter
+    private var nfcAdapter: NfcAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,25 +28,25 @@ class MainActivity : ComponentActivity() {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         if (nfcAdapter == null) {
             Toast.makeText(this, "NFC is not available on this device.", Toast.LENGTH_SHORT).show()
-            finish()
-            return
         }
     }
 
     override fun onResume() {
         super.onResume()
-        val intent = Intent(this, javaClass).apply {
-            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        nfcAdapter?.let {
+            val intent = Intent(this, javaClass).apply {
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            }
+            val pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_MUTABLE
+            )
+            it.enableForegroundDispatch(this, pendingIntent, null, null)
         }
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent, PendingIntent.FLAG_MUTABLE
-        )
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
     }
 
     override fun onPause() {
         super.onPause()
-        nfcAdapter.disableForegroundDispatch(this)
+        nfcAdapter?.disableForegroundDispatch(this)
     }
 
     override fun onNewIntent(intent: Intent) {
@@ -60,13 +61,15 @@ class MainActivity : ComponentActivity() {
             NfcAdapter.ACTION_TAG_DISCOVERED == action) {
 
             val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+            Log.d("NFC", "Tag: $tag")
             tag?.let { nfcTag ->
                 val ndef = Ndef.get(nfcTag)
+                Log.d("NFC", "NDEF: $ndef")
                 ndef?.connect()
                 val ndefMessage = ndef?.ndefMessage
                 val payload = ndefMessage?.records?.get(0)?.payload
                 val productId = String(payload ?: ByteArray(0)).trim()
-
+                Log.d("NFC", "Product ID: $productId")
                 addToCart(this, productId)
                 ndef?.close()
             }
