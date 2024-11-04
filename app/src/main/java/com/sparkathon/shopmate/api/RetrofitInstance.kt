b238223d3407feafe6a2ce00740b0a88a4e0392d
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2024-11-04 20:41:57 +0530

    upd

--- app/src/main/java/com/sparkathon/shopmate/api/RetrofitInstance.kt
+++ app/src/main/java/com/sparkathon/shopmate/api/RetrofitInstance.kt
@@ -7,7 +7,7 @@ object RetrofitInstance {
 
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://143.110.182.65:5000/")
+            .baseUrl("http://64.227.156.56:5000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
